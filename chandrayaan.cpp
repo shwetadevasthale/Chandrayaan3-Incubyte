c8a4861cf7fb5f77cd02e7053180d70cea90b53f
Shweta Devasthale <80617199+shwetadevasthale@users.noreply.github.com>
2023-09-15 14:24:24 +0530

    handled invalid cases

--- chandrayaan.cpp
+++ chandrayaan.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
+#include <stdexcept> // Include for exception handling
 using namespace std;
 
-class Chandrayaan3 {
+class Chandrayaan3
+{
 private:
     int x, y, z;
     char direction;
@@ -12,41 +14,33 @@ public:
         : x(x), y(y), z(z), direction(direction) {}
 
     // Move the spacecraft forward or backward
-    void move(char command) {
-        if (command == 'f') {
-            // Move forward based on the current direction
-            if (direction == 'N')
-                y++;
-            else if (direction == 'S')
-                y--;
-            else if (direction == 'E')
-                x++;
-            else if (direction == 'W')
-                x--;
-            else if (direction == 'U')
-                z++;
-            else if (direction == 'D')
-                z--;
-        } else if (command == 'b') {
-            // Move backward based on the current direction
-            if (direction == 'N')
-                y--;
-            else if (direction == 'S')
-                y++;
-            else if (direction == 'E')
-                x--;
-            else if (direction == 'W')
-                x++;
-            else if (direction == 'U')
-                z--;
-            else if (direction == 'D')
-                z++;
+    void move(char command)
+    {
+        if (command == 'f')
+        {
+            // Check boundaries before moving forward
+            if (!isValidMove(x, y, z, direction, 1))
+            {
+                throw invalid_argument("Invalid move command.");
+            }
+            moveForward();
+        }
+        else if (command == 'b')
+        {
+            // Check boundaries before moving backward
+            if (!isValidMove(x, y, z, direction, -1))
+            {
+                throw invalid_argument("Invalid move command.");
+            }
+            moveBackward();
         }
     }
 
     // Turn the spacecraft left or right
-    void turn(char command) {
-        if (command == 'l') {
+    void turn(char command)
+    {
+        if (command == 'l')
+        {
             prev = direction;
             // Rotate left 90 degrees
             if (direction == 'N')
@@ -57,7 +51,8 @@ public:
                 direction = 'N';
             else if (direction == 'W')
                 direction = 'S';
-            else {
+            else
+            {
                 // Handle 'l' command when spacecraft is tilted (U or D)
                 if (prev == 'E')
                     direction = 'N';
@@ -68,7 +63,9 @@ public:
                 else if (prev == 'S')
                     direction = 'E';
             }
-        } else if (command == 'r') {
+        }
+        else if (command == 'r')
+        {
             prev = direction;
             // Rotate right 90 degrees
             if (direction == 'N')
@@ -79,7 +76,8 @@ public:
                 direction = 'S';
             else if (direction == 'W')
                 direction = 'N';
-            else {
+            else
+            {
                 // Handle 'r' command when spacecraft is tilted (U or D)
                 if (prev == 'E')
                     direction = 'S';
@@ -90,51 +88,151 @@ public:
                 else if (prev == 'S')
                     direction = 'W';
             }
-        } else if (command == 'u') {
-            // Tilt the spacecraft upwards
-            if (direction == 'N' || direction == 'S' || direction == 'E' || direction == 'W')
-                direction = 'U';
-            else if (direction == 'U')
-                direction = 'S';
-            else if (direction == 'D')
-                direction = 'N';
-        } else {
-            // Tilt the spacecraft downwards
-            if (direction == 'N' || direction == 'S' || direction == 'E' || direction == 'W')
-                direction = 'D';
-            else if (direction == 'U')
-                direction = 'N';
-            else if (direction == 'D')
-                direction = 'S';
+        }
+        else if (command == 'u')
+        {
+            // Check boundaries before tilting up
+            if (!isValidTilt(direction, 'U'))
+            {
+                throw invalid_argument("Invalid tilt up command.");
+            }
+            tiltUp();
+        }
+        else
+        {
+            // Check boundaries before tilting down
+            if (!isValidTilt(direction, 'D'))
+            {
+                throw invalid_argument("Invalid tilt down command.");
+            }
+            tiltDown();
         }
     }
 
     // Execute a sequence of commands
-    void executeCommands(const char commands[], int numCommands) {
-        for (int i = 0; i < numCommands; i++) {
+    void executeCommands(const char commands[], int numCommands)
+    {
+        for (int i = 0; i < numCommands; i++)
+        {
             char command = commands[i];
-            if (command == 'f' || command == 'b') {
+            if (command == 'f' || command == 'b')
+            {
                 move(command);
-            } else if (command == 'l' || command == 'r' || command == 'u' || command == 'd') {
+            }
+            else if (command == 'l' || command == 'r' || command == 'u' || command == 'd')
+            {
                 turn(command);
+            }
+            else
+            {
+                throw invalid_argument("Invalid command.");
             }
         }
     }
 
     // Get the final position of the spacecraft
-    void getPosition(int& finalX, int& finalY, int& finalZ) const {
+    void getPosition(int &finalX, int &finalY, int &finalZ) const
+    {
         finalX = x;
         finalY = y;
         finalZ = z;
     }
 
     // Get the final direction of the spacecraft
-    char getDirection() const {
+    char getDirection() const
+    {
         return direction;
+    }
+
+private:
+    void moveForward()
+    {
+        if (direction == 'N')
+            y++;
+        else if (direction == 'S')
+            y--;
+        else if (direction == 'E')
+            x++;
+        else if (direction == 'W')
+            x--;
+        else if (direction == 'U')
+            z++;
+        else if (direction == 'D')
+            z--;
+    }
+
+    void moveBackward()
+    {
+        if (direction == 'N')
+            y--;
+        else if (direction == 'S')
+            y++;
+        else if (direction == 'E')
+            x--;
+        else if (direction == 'W')
+            x++;
+        else if (direction == 'U')
+            z--;
+        else if (direction == 'D')
+            z++;
+    }
+
+    void tiltUp()
+    {
+        if (direction == 'N' || direction == 'S' || direction == 'E' || direction == 'W')
+            direction = 'U';
+        else if (direction == 'U')
+            direction = 'S';
+        else if (direction == 'D')
+            direction = 'N';
+    }
+
+    void tiltDown()
+    {
+        if (direction == 'N' || direction == 'S' || direction == 'E' || direction == 'W')
+            direction = 'D';
+        else if (direction == 'D')
+            direction = 'S';
+        else if (direction == 'U')
+            direction = 'N';
+    }
+
+    // Check if the move is valid within boundaries
+    bool isValidMove(int x, int y, int z, char direction, int step)
+    {
+        // Check boundaries for x, y, and z
+        // You can define the boundaries according to your requirements
+        // For example, if x, y, and z must be within [-10, 10]:
+        if (x + step < -10 || x + step > 10 || y + step < -10 || y + step > 10 || z + step < -10 || z + step > 10)
+        {
+            return false;
+        }
+        return true;
+    }
+
+    // Check if the tilt command is valid
+    bool isValidTilt(char currentDirection, char tiltDirection)
+    {
+        // Check if the spacecraft is already tilted (U or D)
+        if (currentDirection == 'U' || currentDirection == 'D')
+        {
+            // If it's already tilted, a new tilt command in the same direction is not valid
+            return false;
+        }
+
+        // Check if the tilt direction is 'U' or 'D'
+        if (tiltDirection != 'U' && tiltDirection != 'D')
+        {
+            // Invalid tilt direction
+            return false;
+        }
+        // If none of the above conditions are met, the tilt command is valid
+        return true;
     }
 };
 
-int main() {
+int main()
+{
     int initialX = 0, initialY = 0, initialZ = 0;
     char initialDirection = 'N';
     char commands[] = {'f', 'r', 'u', 'b', 'l'};
